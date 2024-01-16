93a3059698be2522cf37d72d0bf2a5526f56a861
patchaiyammal <patchaiyammal.r@cognizant.com>
2024-01-16 14:17:22 +0530

    added

---
+++ src/main/java/com/stackroute/awsdemo1/Awsdemo1Application.java
@@ -0,0 +1,13 @@
+package com.stackroute.awsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awsdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awsdemo1Application.class, args);
+	}
+
+}
